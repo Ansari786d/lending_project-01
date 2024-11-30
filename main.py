6be63864c506d9284f2d4c9ba28d730d8412b770
ansari <shahidansari9341@gmail.com>
2024-11-30 18:48:58 +0530

    Adding a initialize set of files

---
+++ main.py
@@ -0,0 +1,21 @@
+# Scenario 1
+# We are starting a project from github repository 
+
+# Step 1
+# 1. git clone https://github.com/Ansari786d/Glassdore-Scrapper-Using-Selenium.git
+
+# any folder which is get tracked by git will have a .git folder
+# orgin/main (github code)
+
+#1. make changes to existing files / added new files(unstaged)
+#2. we have to stage the changes (staged)
+#3. we can commit all the staged changes 
+
+
+# git add . (to stage all the files in currrent diretory) or give a specific file name 
+# git commit -m "my messages" (to commit it) (here our code is tracked locally)
+# git push origin main  (to push the new changed code into my github repo on main branch)
+
+# scenario 2 we are starting a poject from local 
+
+# 1.git init means we want git to track all the changes on this repository or folder going forward 
