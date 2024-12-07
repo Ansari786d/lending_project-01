2bc780189ad15ada539c16c6a866ef4ecc36e413
ansari <shahidansari9341@gmail.com>
2024-12-07 12:32:23 +0530

    updating the main file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Scenario 1
+#### Scenario 1
 # We are starting a project from github repository 
 
 # Step 1
@@ -16,18 +16,19 @@
 # git commit -m "my messages" (to commit it) (here our code is tracked locally)
 # git push origin main  (to push the new changed code into my github repo on main branch)
 
-# scenario 2 we are starting a poject from local 
 
+#-----------------------------------------------------------------------------------------------
+# scenario 2 we are starting a poject from local
+ 
 # 1.git init means we want git to track all the changes on this repository or folder going forward 
 
-# When we clone a remote project the origin is referring to that.
+## When we clone a remote project the origin is referring to that.
+#  but when we start developing project on local we need to set the origin 
 
-# but when we start developing project on local we need to set the origin 
-
-# to do this we have a command 
+## to do this we have a command 
 # git remote add origin my_repo_url meaning my origin to this remote(github repo)
 
-# to know where our origin point we can check by
+### to know where our origin point we can check by
 
 # git remote -v 
 
@@ -36,6 +37,11 @@
 # if you are getting your branch as master you need to changed it to main in order to push the code to github otherise it 
 # will throw error (in github it has named "mian")
 
-# to rename 
+### to rename 
 # git branch -M main (whatever branch we are in will named as main)
+
+### Instead of writing everytime git push origin main we can do
+#  git push -u origin main (-u means upstream)
+
+# now if we just do git push (now it will assum as git push origin main )
 
